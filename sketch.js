5883db9a519ba4eacac184242a74334fd8898461
Wiktwn <136186152+Wiktwn@users.noreply.github.com>
2023-09-15 09:48:08 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,50 @@
 let branches = [];
-let fractalAngle1 = 45;
-let fractalAngle2 = 45;
-let fractalLength = 100
+let fractalAngle1 = 90;
+let fractalAngle2 = 90;
+let fractalLength = 100;
+let fractalLengthFraction1 = 1.5;
+let fractalLengthFraction2 = 1.5;
 let depth = 10;
-let pv1;let pv2;let pv3;
 
 function setup() {
   canvas = createCanvas(windowWidth/1.2, windowHeight/1.2);
   canvas.position((windowWidth-width)/2, (windowHeight-height)/2);
   strokeWeight(3);
   tree(createVector(width/2, height), fractalLength, 180, depth);
-  s1 = createSlider(0, 180, 30, 0);
-  s2 = createSlider(0, 180, 30, 0);
-  s3 = createSlider(0, height/2, 100, 0);
-  s1.size(100);s2.size(100);s3.size(100);
+  s1 = createSlider(0, 180, fractalAngle1, 0);
+  s2 = createSlider(0, 180, fractalAngle2, 0);
+  s3 = createSlider(0, height/2, fractalLength, 0);
+  s4 = createSlider(1, 2, fractalLengthFraction1, 0);
+  s5 = createSlider(1, 2, fractalLengthFraction2, 0);
+  s1.size(100);s2.size(100);s3.size(100);s4.size(100);s5.size(100);
   s1.position(windowWidth/2+50, windowHeight-40);
-  s2.position(windowWidth/2-50, windowHeight-40);
-  s3.position(windowWidth/2-150, windowHeight-40);
-  pv1 = s1.value();pv2 = s2.value();pv3 = s3.value();
+  s2.position(windowWidth/2-150, windowHeight-40);
+  s3.position(windowWidth/2-50, windowHeight-40);
+  s4.position(windowWidth/2-150, windowHeight-20);
+  s5.position(windowWidth/2+50, windowHeight-20);
   
-  inp = createInput("");
+  inp = createInput("10");
   inp.size(50);
   inp.position(windowWidth/2-25, 10);
   inp.input(inputDepth);
+  s1.input(sliderChange);s2.input(sliderChange);s3.input(sliderChange);
+  s4.input(sliderChange);s5.input(sliderChange);
+  
+  noLoop();
 }
 
 function windowResized() {
   branches = [];
   tree(createVector(width/2, height), fractalLength, 180, depth);
   resizeCanvas(windowWidth/1.2, windowHeight/1.2);
-  canvas.position((windowWidth-width)/2, (windowHeight-height)/2);
-  s1.size(100);
-  s2.size(100);
-  s3.size(100);
   s1.position(windowWidth/2+50, windowHeight-40);
-  s2.position(windowWidth/2-50, windowHeight-40);
-  s3.position(windowWidth/2-150, windowHeight-40);
+  s2.position(windowWidth/2-150, windowHeight-40);
+  s3.position(windowWidth/2-50, windowHeight-40);
+  s4.position(windowWidth/2-150, windowHeight-20);
+  s5.position(windowWidth/2+50, windowHeight-20);
 }
 
 function draw() {
-  
   background(220);
   for (let b of branches) {
     let x1 = b[0]; let y1 = b[1];
@@ -47,16 +52,6 @@ function draw() {
     strokeWeight(b[4]);
     line(x1, y1, x2, y2);
   }
-  if (s1.value()!=pv1 || s2.value()!=pv2 || s3.value()!=pv3) {
-    branches = [];
-    fractalAngle1 = s1.value();
-    fractalAngle2 = s2.value();
-    fractalLength = s3.value();
-    tree(createVector(width/2, height), fractalLength, 180, depth);
-  }
-  pv1 = s1.value();
-  pv2 = s2.value();
-  pv3 = s3.value();
 }
 
 function tree(start, len, angle, depth=10, stroke=3) {
@@ -66,8 +61,8 @@ function tree(start, len, angle, depth=10, stroke=3) {
   branches.push([start.x, start.y, x+start.x, y+start.y, stroke])
   if (depth > 0) {
     let end = createVector(x+start.x, y+start.y);
-    tree(end, len/1.5, angle+fractalAngle1, depth-1, stroke-0.2);
-    tree(end, len/1.5, angle-fractalAngle2, depth-1, stroke-0.2);
+    tree(end, len/fractalLengthFraction1, angle+fractalAngle1, depth-1, stroke-0.2);
+    tree(end, len/fractalLengthFraction2, angle-fractalAngle2, depth-1, stroke-0.2);
   }
 }
 
@@ -75,4 +70,16 @@ function inputDepth() {
   depth = int(this.value());
   branches = [];
   tree(createVector(width/2, height), fractalLength, 180, depth);
+  redraw();
+}
+
+function sliderChange() {
+  fractalAngle1 = s1.value();
+  fractalAngle2 = s2.value();
+  fractalLength = s3.value();
+  fractalLengthFraction1 = s4.value();
+  fractalLengthFraction2 = s5.value();
+  branches = [];
+  tree(createVector(width/2, height), fractalLength, 180, depth);
+  redraw();
 }
